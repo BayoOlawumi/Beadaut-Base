23d95eaae91b77e3996802ac1a7414e556092194
BayoOlawumi <olawumiebayo@gmail.com>
2024-04-13 11:32:41 +0900

    add major

---
+++ 11. visualization exploration/pages/page_1.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.sidebar.markdown("# Do Prediction Here ❄️")
+
+st.title('Modelling Session')
+
+st.subheader(
+    """
+        **We used Random Forest!**
+
+""")
